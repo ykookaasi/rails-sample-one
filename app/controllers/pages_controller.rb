0ae8fd57f6175e2d016f3013231e2aed3d5e4670
Yoko Kasai <ykookaasi@gmail.com>
2020-11-15 20:44:47 +0100

    Create about contact home pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,16 @@ class PagesController < ApplicationController
   end
 
   def contact
+    @date = Date.today.strftime("%a, %b %d")
+    @members = [ 'yoko', 'grzegorz', 'pedro', 'luis', 'stan' ]
+    if params[:member]
+      @members = @members.select { |member| member.start_with?(params[:member]) }
+      end
+     end
+
   end
 
   def home
+    @date = Date.today.strftime("%a, %b %d")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-  get 'pages/contact'
-  get 'pages/home'
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
