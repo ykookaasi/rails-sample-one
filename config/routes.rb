37fb2e699a5dc034fe7d140b4b76f7f0aa19e11d
Yoko Kasai <ykookaasi@gmail.com>
2020-11-15 20:32:51 +0100

    Configure cloudinary and create root pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+  get 'pages/contact'
+  get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
